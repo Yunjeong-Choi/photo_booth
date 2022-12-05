3df70a2f3caf8c514543ac2831639105976cc0fc
Yunjeong Choi <m.yunjeong.choi@gmail.com>
2022-12-05 17:11:21 +0900

    [PB-4] Home 페이지 UI 생성

---
+++ src/components/ContainerWithBackground.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import background from "../assets/img_photo_booth_background.svg";
+
+interface ContainerWithBackgroundProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+const ContainerWithBackground = (props: ContainerWithBackgroundProps) => {
+  return <Container className={props.className}>{props.children}</Container>;
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+  background: center / contain no-repeat url(${background}), black;
+
+  text-align: center;
+`;
+
+export default ContainerWithBackground;
