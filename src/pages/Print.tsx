b9a477b5eadb3ee1d9811c172bd43a726aa25c1c
Yunjeong Choi <m.yunjeong.choi@gmail.com>
2022-12-07 08:40:44 +0900

    [PB-6] 폰트 적용

--- src/pages/Print.tsx
+++ src/pages/Print.tsx
@@ -49,8 +49,8 @@ const Print = () => {
         <SectionTitle>프린트 하기</SectionTitle>
         <Notice>
           {`① 아래 "프린트" 버튼을 누르고,
-          ② [인쇄하고 싶은 사진의 수] + [편지용 추가 1장] 설정
-          ③ "인쇄" 버튼 클릭하면 끝!`}
+            ② [소장용 N장] + [편지용 추가 1장] 설정
+            ③ "인쇄" 버튼 클릭하면 끝!`}
         </Notice>
         <PrintButton handleClickButton={printDiv}>프린트</PrintButton>
       </NoticeSection>
@@ -59,18 +59,19 @@ const Print = () => {
 };
 
 const StyledContainer = styled(ContainerWithBackground)`
-  gap: 8rem;
+  gap: 6rem;
 `;
 
 const NoticeSection = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 5rem;
+  gap: 3rem;
 `;
 
 const SectionTitle = styled.div`
   font-size: 6rem;
+  font-weight: 800;
 `;
 
 const Notice = styled.p`
