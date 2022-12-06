7a6ca1ddc4daef76f56eeb545c25814ee6a35f2b
Yunjeong Choi <m.yunjeong.choi@gmail.com>
2022-12-06 09:29:48 +0900

    [PB-5] Camera 페이지 UI 생성 & 촬영 타이머 구현

--- src/pages/Camera.tsx
+++ src/pages/Camera.tsx
@@ -1,20 +1,60 @@
-import { useRef, useCallback, useState } from "react";
+import { useRef, useCallback, useState, useEffect } from "react";
 import styled from "styled-components";
 import Webcam from "react-webcam";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import ContainerWithBackground from "../components/ContainerWithBackground";
 
-const photoWidth = 450;
+const photoWidth = 490;
 const photoHeight = 700;
 
-function Camera() {
-  const webcamRef = useRef<Webcam>(null);
-  const [imageList, setImageList] = useState<string[]>([]);
+const videoConstraints = {
+  width: photoWidth,
+  height: photoHeight,
+  facingMode: "user",
+};
 
-  const videoConstraints = {
-    width: photoWidth,
-    height: photoHeight,
-    facingMode: "user",
-  };
+function Camera() {
+  const navigate = useNavigate();
+  const webcamRef = useRef<Webcam>(null);
+  const resultImageRef = useRef<HTMLImageElement>(null);
+  const timerRef = useRef<any>(null);
+  const [imageList, setImageList] = useState<string[]>([]);
+  const [imageCount, setImageCount] = useState(1);
+  const [countdown, setCountdown] = useState(10);
+
+  useEffect(() => {
+    startCountdown();
+  }, []);
+
+  useEffect(() => {
+    if (countdown <= 0 && timerRef.current) {
+      clearInterval(timerRef.current);
+      timerRef.current = null;
+      capture();
+    }
+  }, [countdown]);
+
+  useEffect(() => {
+    if (imageList.length > 0 && imageList.length < 6) {
+      setTimeout(() => {
+        setImageCount((prev) => prev + 1);
+        setCountdown(10);
+        startCountdown();
+      }, 3000);
+    } else if (imageList.length >= 6) {
+      setTimeout(() => {
+        navigate("/gallery", { state: { imageList } });
+      }, 3000);
+    }
+  }, [imageList.length]);
+
+  const startCountdown = useCallback(() => {
+    if (timerRef.current === null) {
+      timerRef.current = setInterval(() => {
+        setCountdown((prev) => prev - 1);
+      }, 500); //TODO: 1250
+    }
+  }, []);
 
   const capture = useCallback(() => {
     if (webcamRef.current) {
@@ -25,14 +65,27 @@ function Camera() {
           copy.push(captureResult);
           return copy;
         });
+        showResult(captureResult);
       }
     }
   }, [webcamRef]);
 
+  const showResult = (result: string) => {
+    if (resultImageRef.current) {
+      resultImageRef.current.setAttribute("src", result);
+      resultImageRef.current.style.display = "block";
+      setTimeout(() => {
+        if (resultImageRef.current) {
+          resultImageRef.current.style.display = "none";
+        }
+      }, 3000);
+    }
+  };
+
   return (
-    <Container>
+    <ContainerWithBackground>
       <CameraContainer>
-        <StyledWebcam
+        <Webcam
           ref={webcamRef}
           audio={false}
           width={photoWidth}
@@ -42,33 +95,37 @@ function Camera() {
             ...videoConstraints,
           }}
         />
-        <button onClick={capture}>Capture photo</button>
+        <Result ref={resultImageRef} src={""} alt={"shooting result"} />
       </CameraContainer>
-      <button>
-        <Link to="/gallery" state={{ imageList }}>
-          사진 고르기
-        </Link>
-      </button>
-    </Container>
+      <Countdown>
+        <p>{`6장 중 ${imageCount}번째 촬영`}</p>
+        <h2>{countdown}</h2>
+      </Countdown>
+    </ContainerWithBackground>
   );
 }
 
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 50px;
-
-  width: 100%;
-  height: 100%;
-`;
-
 const CameraContainer = styled.div`
-  flex-shrink: 0;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
+  position: relative;
 `;
 
-const StyledWebcam = styled(Webcam)``;
+const Result = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  display: none;
+
+  width: ${photoWidth}px;
+  height: ${photoHeight}px;
+`;
+
+const Countdown = styled.div`
+  position: absolute;
+  top: 6rem;
+  left: 8rem;
+
+  font-size: 5rem;
+`;
 
 export default Camera;
