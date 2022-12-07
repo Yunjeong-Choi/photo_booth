ab1f0b6ef75fbf62453a9111b3c7e4e683f7ccba
Yunjeong Choi <m.yunjeong.choi@gmail.com>
2022-12-07 09:20:18 +0900

    [PB-8] 촬영 시작 전 타이머 추가

--- src/pages/Camera.tsx
+++ src/pages/Camera.tsx
@@ -17,29 +17,39 @@ function Camera() {
   const navigate = useNavigate();
   const webcamRef = useRef<Webcam>(null);
   const resultImageRef = useRef<HTMLImageElement>(null);
-  const timerRef = useRef<any>(null);
+  const initialCountdownRef = useRef<any>(null);
+  const shootingCountdownRef = useRef<any>(null);
   const [imageList, setImageList] = useState<string[]>([]);
   const [imageCount, setImageCount] = useState(1);
-  const [countdown, setCountdown] = useState(10);
+  const [initialCountdown, setInitialCountdown] = useState(5);
+  const [shootingCountdown, setShootingCountdown] = useState(10);
 
   useEffect(() => {
-    startCountdown();
+    startInitialCountdown();
   }, []);
 
   useEffect(() => {
-    if (countdown <= 0 && timerRef.current) {
-      clearInterval(timerRef.current);
-      timerRef.current = null;
+    if (initialCountdown <= 0 && initialCountdownRef.current) {
+      clearInterval(initialCountdownRef.current);
+      initialCountdownRef.current = null;
+      startShootingCountdown();
+    }
+  }, [initialCountdown]);
+
+  useEffect(() => {
+    if (shootingCountdown <= 0 && shootingCountdownRef.current) {
+      clearInterval(shootingCountdownRef.current);
+      shootingCountdownRef.current = null;
       capture();
     }
-  }, [countdown]);
+  }, [shootingCountdown]);
 
   useEffect(() => {
     if (imageList.length > 0 && imageList.length < 6) {
       setTimeout(() => {
         setImageCount((prev) => prev + 1);
-        setCountdown(10);
-        startCountdown();
+        setShootingCountdown(10);
+        startShootingCountdown();
       }, 3000); // TODO: settimeout 안쓰고 순서 맞추기?
     } else if (imageList.length >= 6) {
       setTimeout(() => {
@@ -48,11 +58,19 @@ function Camera() {
     }
   }, [imageList.length]);
 
-  const startCountdown = useCallback(() => {
-    if (timerRef.current === null) {
-      timerRef.current = setInterval(() => {
-        setCountdown((prev) => prev - 1);
-      }, 100); //TODO: 1250
+  const startInitialCountdown = useCallback(() => {
+    if (initialCountdownRef.current === null) {
+      initialCountdownRef.current = setInterval(() => {
+        setInitialCountdown((prev) => prev - 1);
+      }, 1250);
+    }
+  }, []);
+
+  const startShootingCountdown = useCallback(() => {
+    if (shootingCountdownRef.current === null) {
+      shootingCountdownRef.current = setInterval(() => {
+        setShootingCountdown((prev) => prev - 1);
+      }, 1250);
     }
   }, []);
 
@@ -100,9 +118,18 @@ function Camera() {
         <Result ref={resultImageRef} src={""} alt={"shooting result"} />
       </CameraContainer>
       <Countdown>
-        <p>{`6장 중 ${imageCount}번째 촬영`}</p>
-        <h2>{countdown}</h2>
-        {/* TODO: 자동으로 이동합니다. */}
+        {initialCountdown > 0 ? (
+          <>
+            <p>{`5초 후에
+                촬영을 시작합니다`}</p>
+            <h3>{initialCountdown}</h3>
+          </>
+        ) : (
+          <>
+            <p>{`6장 중 ${imageCount}번째 촬영`}</p>
+            <h3>{shootingCountdown}</h3>
+          </>
+        )}
       </Countdown>
     </StyledContainer>
   );
@@ -133,6 +160,14 @@ const Countdown = styled.div`
   left: 8rem;
 
   font-size: 5rem;
+
+  > p {
+    white-space: pre-line;
+  }
+
+  > h3 {
+    font-size: 10rem;
+  }
 `;
 
 export default Camera;
